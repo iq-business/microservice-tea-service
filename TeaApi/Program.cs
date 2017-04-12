d03d260491b29fe2c1b2b269e4709a3c873b7d27
Robert Bravery <rbravery@iqbusiness.net>
2017-04-12 22:27:24 +0200

    Initial Commit

---
+++ TeaApi/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace TeaApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables().Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .ConfigureLogging(l => l.AddConsole(config.GetSection("Logging")))
+                .ConfigureServices(s => s.AddRouting())
+                .Configure(app =>
+                {
+                    // define all API endpoints
+                    var tprice = new TeaPrice {Price = 21.95m};
+                    app.UseRouter(r =>
+                    {
+                        r.MapGet("tea", context => context.Response.WriteJson(tprice));
+                    });
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public static class HttpExtensions
+    {
+        private static readonly JsonSerializer Serializer = new JsonSerializer();
+
+        public static Task WriteJson<T>(this HttpResponse response, T obj)
+        {
+            response.ContentType = "application/json";
+            return response.WriteAsync(JsonConvert.SerializeObject(obj));
+        }
+
+        public static async Task<T> ReadFromJson<T>(this HttpContext httpContext)
+        {
+            using (var streamReader = new StreamReader(httpContext.Request.Body))
+            using (var jsonTextReader = new JsonTextReader(streamReader))
+            {
+                var obj = Serializer.Deserialize<T>(jsonTextReader);
+
+                var results = new List<ValidationResult>();
+                if (Validator.TryValidateObject(obj, new ValidationContext(obj), results))
+                {
+                    return obj;
+                }
+
+                httpContext.Response.StatusCode = 400;
+                await httpContext.Response.WriteJson(results);
+
+                return default(T);
+            }
+        }
+    }
+
+    class TeaPrice
+    {
+        public decimal Price { get; set; } 
+    }
+}
